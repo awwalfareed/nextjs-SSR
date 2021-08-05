d0ef5b19bd7c77a9ffa4d7c51e472c958a74b1f5
virenkhembavi <khembaviv@gmail.com>
2021-08-05 11:11:28 +0530

    init

--- pages/Feedback.js
+++ pages/Feedback.js
@@ -3,96 +3,118 @@ import { makeStyles } from '@material-ui/core/styles';
 import React from 'react'
 import styles from '../styles/Feedback.module.css'
 import Checkbox from '@material-ui/core/Checkbox'
+import { useState } from 'react'
 
 const useStyles = makeStyles({
     root: {
         backgroundColor: 'lightblue',
         display: 'flex',
         flexDirection: 'row',
-        //   justifyContent:'flex-end'
-        alignItems: 'left'
+        justifyContent: 'flex-end',
+        // alignItems: 'left'
     },
     like: {
-        width: '20%',
-        height: '30px',
-        borderLeft: '0.5px solid black',
-        borderRight: '1px solid black'
+        width: '19%',
+        height: '40px',
+        border: '0.5px solid black',
+        borderRadius: '0px',
+        fontFamily: 'Encode Sans SC , sans-serif',
+        zIndex: '-1'
+    },
+    txt: {
+        width: '100%'
+    },
+    ugh: {
+        width: '19%',
+        height: '40px',
+        border: '0.5px solid black',
+        borderRadius: '0px',
+        zIndex: '-1'
     }
 });
 
 const Feedback = () => {
     const classes = useStyles();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(name)
+        console.log(email)
+        console.log(trouble)
+        console.log(feature)
+        console.log(suggestion)
+    }
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [trouble, setTrouble] = useState('');
+    const [feature, setFeature] = useState('');
+    const [suggestion, setSuggestion] = useState('');
+    // const [selectedNumber, setSelectedNumber] = useState(0);
+
+    // const selectNumber = numberSelected => {
+    //     setSelectedNumber(numberSelected)
+    // }
     return (
-        <div className={styles.container}>
-            <div className={styles.text}>
-                <h3 className={styles.send}>Send Us Your Feedback!</h3>
-            </div>
-            <div className={styles.txtfield}>
-                <h5 className={styles.add}>Name</h5>
-                <TextField className={styles.name} />
-                <h5 className={styles.add}>Email</h5>
-                <TextField className={styles.name} />
-            </div>
-            <div className={styles.txtfield}>
-                <h4 className={styles.easy}>How easy was it use our website?</h4>
-                <div className={styles.poor}>
-                    <h3 className={styles.exc}>Poor</h3>
-                    <h3 className={styles.exc}>Excellent</h3>
+        <form method="POST" onSubmit={handleSubmit}>
+            <div className={styles.main}>
+                <h4 className={styles.headline} >Send Us Your Feedback!</h4>
+                <div className={styles.feed}>
+                    <h3 className={styles.text}>Name</h3>
+                    <TextField className={classes.txt} required label="Tell Us.." value={name} onChange={(e) => setName(e.target.value)} />
+                    <h3 className={styles.text}>Email</h3>
+                    <TextField className={classes.txt} required label="Tell Us.." value={email} onChange={(e) => setEmail(e.target.value)} />
                 </div>
-
-                <Button className={classes.like}>1</Button>
-                <Button className={classes.like}>2</Button>
-                <Button className={classes.like}>3</Button>
-                <Button className={classes.like}>4</Button>
-                <Button className={classes.like}>5</Button>
-
-            </div>
-            <div className={styles.txtfield}>
-                <h3 className={styles.add}>Are you having trouble finding anything?</h3>
-                <div className={styles.check}>
-                    <div className={styles.checkk}>
-                        <Checkbox className={styles.yes} inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} >
-                        </Checkbox>
-                        <h5 className={styles.easy}>Yes</h5>
+                <div className={styles.feed}>
+                    <h3 className={styles.text}>How easy was it use our website?</h3>
+                    <div className={styles.poor}>
+                        <h2 className={styles.text}>Poor</h2>
+                        <h2 className={styles.text}>Excellent</h2>
                     </div>
-                    <div className={styles.checkk}>
-                        <Checkbox className={styles.yes} inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} >
-                        </Checkbox>
-                        <h5 className={styles.easy}>No</h5>
+                    <Button className={classes.like}>1</Button>
+                    <Button className={classes.like}>2</Button>
+                    <Button className={classes.like}>3</Button>
+                    <Button className={classes.like}>4</Button>
+                    <Button className={classes.like}>5</Button>
+                </div>
+                <div className={styles.feed}>
+                    <h3 className={styles.text}>Are you having any trouble finding anything?</h3>
+                    <div className={styles.exe}>
+                        <Checkbox />
+                        <h5 className={styles.text}>Yes</h5>
                     </div>
-                    <TextField className={styles.name} label="Tell us.." />
+                    <div className={styles.exe}>
+                        <Checkbox />
+                        <h5 className={styles.text}>No</h5>
+                    </div>
+                    <TextField className={classes.txt} required label="Tell Us.." value={trouble} onChange={(e) => setTrouble(e.target.value)} />
                 </div>
-            </div>
-            <div className={styles.txtfield}>
-                <h5 className={styles.add}>Which important feature we should add?</h5>
-                <TextField className={styles.name} label="Tell us.." />
-            </div>
-            <div className={styles.txtfield}>
-                <h5 className={styles.add}>Any suggestion, you want give for making our <br /> website better?</h5>
-                <TextField className={styles.name} label="Tell us.." />
-            </div>
-            <div className={styles.txtfield}>
-                <h5 className={styles.add}>On scale of 1 - 10, Give rating to our website?</h5>
-                <div className={styles.buf}>
-                    <Button className={styles.ugy}>1</Button>
-                    <Button className={styles.ugy}>2</Button>
-                    <Button className={styles.ugy}>3</Button>
-                    <Button className={styles.ugy}>4</Button>
-                    <Button className={styles.ugy}>5</Button>
-                    <Button className={styles.ugy}>6</Button>
-                    <Button className={styles.ugy}>7</Button>
-                    <Button className={styles.ugy}>8</Button>
-                    <Button className={styles.ugy}>9</Button>
-                    <Button className={styles.ugy}>10</Button>
+                <div className={styles.feed}>
+                    <h3 className={styles.text}>Which important feature we should add?</h3>
+                    <TextField className={classes.txt} required label="Tell Us.." value={feature} onChange={(e) => setFeature(e.target.value)} />
                 </div>
+                <div className={styles.feed}>
+                    <h3 className={styles.text}>Any suggestion, you want give for making our <br /> website better?</h3>
+                    <TextField className={classes.txt} required label="Tell Us.." value={suggestion} onChange={(e) => setSuggestion(e.target.value)} />
+                </div>
+                <div className={styles.feed}>
+                    <h3 className={styles.text}>On a scale of 1-5, Give rating to our website?</h3>
+                    <Button className={classes.ugh}>1</Button>
+                    <Button className={classes.ugh}>2</Button>
+                    <Button className={classes.ugh}>3</Button>
+                    <Button className={classes.ugh}>4</Button>
+                    <Button className={classes.ugh}>5</Button>
+                </div>
+                <div className={styles.feed}>
+                    <div className={styles.exe}>
+                        <Checkbox />
+                        <h5 className={styles.text}>Im Not a robot</h5>
+                    </div>
+                </div>
+                <Button className={classes.root} type="submit" onClick={handleSubmit} >Submit</Button>
             </div>
-            <div className={styles.checkk}>
-                <Checkbox className={styles.yes} inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} >
-                </Checkbox>
-                <h5 className={styles.easy}>I'm not a robot</h5>
-            </div>
-            <Button className={classes.root}>Submit</Button>
-        </div>
+        </form>
+
     )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ function MyApp({ Component, pageProps }) {
   return <Layout>
     <Component {...pageProps} />
   </Layout>
+
 }
 
 export default MyApp
