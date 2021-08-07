6a8659d8cbb2b8b0d21b651f6a95fc839a3e734e
virenkhembavi <khembaviv@gmail.com>
2021-08-07 20:34:33 +0530

    post

---
+++ component/Post.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from '../styles/Pages.module.css'
+import Celeb from '../public/images/celeb.jpg'
+import Kpop from '../public/images/kpop.png'
+import Meme from '../public/images/memes.jpg'
+import Singer from '../public/images/singer.jpg'
+import Athlete from '../public/images/atheles.jpg'
+import Ipl from '../public/images/ipl.jpg'
+import Holly from '../public/images/hollywood.jpg'
+import Buss from '../public/images/bussiness.jpg'
+
+const Post = () => {
+    return (
+        <div className={styles.celeb}>
+            <div className={styles.divde}>
+                <div className={styles.bg}>
+                    <Image src={Celeb} alt="Celebrity" className={styles.cel} height="70px" width="70px" />
+                </div>
+                <h5 className={styles.title}>CELEBRITY</h5>
+            </div>
+            <div className={styles.divde}>
+                <div className={styles.bg}>
+                    <Image src={Kpop} alt="Celebrity" className={styles.cel} height="70px" width="70px" />
+                </div>
+                <h5 className={styles.title}>K-POP</h5>
+            </div>
+            <div className={styles.divde}>
+                <div className={styles.bg}>
+                    <Image src={Meme} alt="Celebrity" className={styles.cel} height="70px" width="70px" />
+                </div>
+                <h5 className={styles.title}>MEMES</h5>
+            </div>
+            <div className={styles.divde}>
+                <div className={styles.bg}>
+                    <Image src={Singer} alt="Celebrity" className={styles.cel} height="70px" width="70px" />
+                </div>
+                <h5 className={styles.title}>SINGER</h5>
+            </div>
+            <div className={styles.divde}>
+                <div className={styles.bg}>
+                    <Image src={Athlete} alt="Celebrity" className={styles.cel} height="70px" width="70px" />
+                </div>
+                <h5 className={styles.title}>ATHELETES </h5>
+            </div>
+            <div className={styles.divde}>
+                <div className={styles.bg}>
+                    <Image src={Ipl} alt="Celebrity" className={styles.cel} height="70px" width="70px" />
+                </div>
+                <h5 className={styles.title}>IPL</h5>
+            </div>
+            <div className={styles.divde}>
+                <div className={styles.bg}>
+                    <Image src={Holly} alt="Celebrity" className={styles.cel} height="70px" width="70px" />
+                </div>
+                <h5 className={styles.title}>HOLLYWOOD</h5>
+            </div>
+            <div className={styles.divde}>
+                <div className={styles.bg}>
+                    <Image src={Buss} alt="Celebrity" className={styles.cel} height="70px" width="70px" />
+                </div>
+                <h5 className={styles.title}>BUSSINESS</h5>
+            </div>
+        </div>
+
+    )
+}
+
+export default Post
--- pages/post.js
+++ pages/post.js
@@ -1,5 +1,12 @@
 import React from 'react'
+import Post from '../component/Post'
+import Image from 'next/image'
 import styles from '../styles/Pages.module.css'
+import Abs from '../public/images/Ab.jpg'
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import ModeCommentOutlinedIcon from '@material-ui/icons/ModeCommentOutlined';
+
+
 const post = () => {
     return (
         <div className={styles.container}>
@@ -9,8 +16,73 @@ const post = () => {
                     <h3 className={styles.mor}>MORE SPACE+</h3>
                 </div>
                 <div>
-                    <div className={styles.celeb}>
-                        hey
+                    <Post />
+                </div>
+                <div className={styles.tab}>
+                    <div className={styles.tab1}>
+                        <h3 className={styles.post}>Post</h3>
+                        <h3 className={styles.story}>Story</h3>
+                        <h3 className={styles.story}>Igtv</h3>
+                    </div>
+                </div>
+                <div className={styles.ntg}>
+                    <h2 className={styles.popular}>Popular Celebrity Posts</h2>
+                </div>
+                <div className={styles.mov}>
+
+                    <div className={styles.card}>
+                        <div className={styles.nav}>
+                            <Image src={Abs} className={styles.cardImg} width="70px" height="70px" />
+                            <div className={styles.popu}>
+                                <h4 className={styles.Ab}>Amitabh Bachchan</h4>
+                                <h5 className={styles.sec}> 3 Sec ago.</h5>
+                            </div>
+                        </div>
+                        <div className={styles.Abc}>
+                            <Image src={Abs} className={styles.celebImg} width="500px" height="230px" />
+                        </div>
+                        <div className={styles.comm}>
+                            <FavoriteBorderIcon className={styles.like} />
+                            <span className={styles.num}>4566</span>
+                            <ModeCommentOutlinedIcon className={styles.like} />
+                            <span className={styles.num}>566</span>
+                        </div>
+                        <div className={styles.info}>
+                            <p className={styles.info1}>
+                                # Repost <span className={styles.spn}>@ vkofficial</span><br />
+                                As we embark on our new journey, aiming to <br />
+                                empower each ans every individual and <br />
+                                community, we strive to enrich lives.
+
+                            </p>
+                        </div>
+                    </div>
+                    <div className={styles.card}>
+                        <div className={styles.nav}>
+                            <Image src={Abs} className={styles.cardImg} width="70px" height="70px" />
+                            <div className={styles.popu}>
+                                <h4 className={styles.Ab}>Amitabh Bachchan</h4>
+                                <h5 className={styles.sec}> 3 Sec ago.</h5>
+                            </div>
+                        </div>
+                        <div className={styles.Abc}>
+                            <Image src={Abs} className={styles.celebImg} width="500px" height="230px" />
+                        </div>
+                        <div className={styles.comm}>
+                            <FavoriteBorderIcon className={styles.like} />
+                            <span className={styles.num}>4566</span>
+                            <ModeCommentOutlinedIcon className={styles.like} />
+                            <span className={styles.num}>566</span>
+                        </div>
+                        <div className={styles.info}>
+                            <p className={styles.info1}>
+                                # Repost <span className={styles.spn}>@ vkofficial</span><br />
+                                As we embark on our new journey, aiming to <br />
+                                empower each ans every individual and <br />
+                                community, we strive to enrich lives.
+
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
